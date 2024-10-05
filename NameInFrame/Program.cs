5585a76d463bd0ab53d95679fbed1012c18eac61
Feaxmutch <feaxmutch@gmail.com>
2024-10-05 18:46:45 +0300

    Implemented frame for name

--- NameInFrame/Program.cs
+++ NameInFrame/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            char frameChar = '%';
+
+            Console.Write("Введите своё имя: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write(frameChar);
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.Write(frameChar);
+            }
+
+            Console.WriteLine(frameChar);
+            Console.Write(frameChar);
+            Console.Write(name);
+            Console.WriteLine(frameChar);
+            Console.Write(frameChar);
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.Write(frameChar);
+            }
+
+            Console.WriteLine(frameChar);
+            Console.ReadKey();
         }
     }
 }
